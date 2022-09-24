1a9cc32807e2fd9d9cdccc6ae0e8468c2c33dbb8
Thomas Fettig <thomasmfettig@gmail.com>
2022-09-24 13:37:33 -0600

    Add styling to SavedBeers component, styling is now complete

--- src/components/SavedBeers/SavedBeers.js
+++ src/components/SavedBeers/SavedBeers.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './SavedBeers.css';
 import { getSavedBeers } from '../../api-calls';
 import { useNavigate } from 'react-router-dom';
+import { assignImage } from '../../utility-functions/utility';
 
 const SavedBeers = () => {
   const [savedBeers, setSavedBeers] = useState([]);
@@ -20,22 +21,25 @@ const SavedBeers = () => {
   let beerCards;
   if (savedBeers.length > 0) {
     beerCards = savedBeers.map(beer => {
+      beer.description = 'placeholder';
+      assignImage(beer);
       return (
         <div className='saved-beer-card' key={beer.id}>
-          <p>{beer.name}</p>
-          <p>{beer.tagline}</p>
-          <p>{`ABV: ${beer.abv} IBU: ${beer.ibu}`}</p>
-          <p>{beer.notes}</p>
-          <button onClick={() => selectABeer(beer)}>Brew Me Again!</button>
+          <p className='saved-beer-name'>{beer.name}</p>
+          <p className='saved-beer-tagline'>{beer.tagline}</p>
+          <p className='saved-beer-stats'>{`ABV: ${beer.abv} IBU: ${beer.ibu}`}</p>
+          <p className='saved-beer-notes'>Notes: {beer.notes}</p>
+          <img className='saved-beer-image' src={beer.image} alt='example beer'></img>
+          <button className='brew-again-button' onClick={() => selectABeer(beer)}>Brew Me Again!</button>
         </div>
       );
     });
   } else {
-    beerCards = <p>'You dont have any saved beers yet, find a recipe that sounds good and give it a shot!'</p>
+    beerCards = <p className='no-saved-beers'>You dont have any saved beers yet, find a recipe that sounds good and give it a shot!</p>
   }
 
   return (
-    <section>
+    <section className='saved-beers-section'>
       {beerCards}
     </section>
   )
